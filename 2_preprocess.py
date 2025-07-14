175dd7f335e102f01ac2a7696e80337f59c77f64
namithadeshpande <namithadeshpande@gmail.com>
2025-07-13 22:12:25 -0500

    feature selection file added

--- 2_preprocess.py
+++ 2_preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from sklearn.ensemble import RandomForestRegressor
 
 csv_file = "yf_data_new_features.csv"
 
@@ -12,3 +13,4 @@ feat_df = pd.read_csv(csv_file, parse_dates=['Date'], index_col='Date')
 feat_df = feat_df[(feat_df.index >= START_DATE) & (feat_df.index <= END_DATE)]
 
 feat_df = feat_df.sort_index()
+
