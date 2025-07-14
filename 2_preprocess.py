74cc60336277a93edb4baf363b72cf9ec8fcb1d8
namithadeshpande <namithadeshpande@gmail.com>
2025-07-13 22:01:47 -0500

    new file added

---
+++ 2_preprocess.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+csv_file = "yf_data_new_features.csv"
+
+START_DATE = '2001-01-01'
+END_DATE = '2025-06-30'
+
+# Load the CSV file
+feat_df = pd.read_csv(csv_file, parse_dates=['Date'], index_col='Date')
+feat_df = feat_df[(feat_df.index >= START_DATE) & (feat_df.index <= END_DATE)]
+
+feat_df = feat_df.sort_index()
